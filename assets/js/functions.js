489157c86557c36bf9a11ecc5f71d1d7201accb5
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-08-10 18:03:59 +0200

    Add delete functionality

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -20,6 +20,8 @@ function renderPost () {
                 data-id="${post.id}"
                 >Go somewhere</a
               >
+              <button id="editPost">Edit</button>
+              <button id="deletePost" data-delete="${post.id}">Delete</button>
             </div>
           </div>
         `
@@ -30,43 +32,52 @@ function renderPost () {
 
 document.getElementById('postFeed').addEventListener('click', async (e) => {
   let targetId = e.target.dataset.id
-  let comments = []
-  let post = await fetch(`${URL}/posts/${targetId}`).then(response => response.json())
-  let user = await fetch(`${URL}/users/${post.userId}`).then(response => response.json())
-  await fetch(`${URL}/comments/`).then(response => response.json()).then(data => {
-    data.forEach(comment => {
-      if(targetId == comment.postId) {
-        comments.push(comment)
-      }
+  let targetDelete = e.target.dataset.delete
+  if (targetId) {
+    let comments = []
+    let post = await fetch(`${URL}/posts/${targetId}`).then(response => response.json())
+    let user = await fetch(`${URL}/users/${post.userId}`).then(response => response.json())
+    await fetch(`${URL}/comments/`).then(response => response.json()).then(data => {
+      data.forEach(comment => {
+        if(targetId == comment.postId) {
+          comments.push(comment)
+        }
+      })
     })
-  })
 
-  let modalHTML = `
-  <div class="modal-header">
-    <h2 class="modal-title" id="modalTitle">${post.title}</h2>
-    <button
-      type="button"
-      class="btn-close"
-      data-bs-dismiss="modal"
-      aria-label="Close"
-    ></button>
-  </div>
-  <div id="modalBody" class="modal-body">
-    <p>${post.body}</p>
-    <h3>user</h3>
-    <p>${user.name}</p>
-    <a href="mailto:${user.email}">${user.email}</a>
-    <div id="commentsContainer">
+    let modalHTML = `
+    <div class="modal-header">
+      <h2 class="modal-title" id="modalTitle">${post.title}</h2>
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="modal"
+        aria-label="Close"
+      ></button>
     </div>
-  </div>
-  <div class="modal-footer">
-    <button type="button" id="loadComments" class="btn btn-primary">
-      Load comments
-    </button>
-  </div>
-  `
-  document.getElementById('myModal').innerHTML = modalHTML
-  document.getElementById('loadComments').addEventListener('click', () => loadComments(comments))
+    <div id="modalBody" class="modal-body">
+      <p>${post.body}</p>
+      <h3>user</h3>
+      <p>${user.name}</p>
+      <a href="mailto:${user.email}">${user.email}</a>
+      <div id="commentsContainer">
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button type="button" id="loadComments" class="btn btn-primary">
+        Load comments
+      </button>
+    </div>
+    `
+    document.getElementById('myModal').innerHTML = modalHTML
+    document.getElementById('loadComments').addEventListener('click', () => loadComments(comments))
+  }
+  if (targetDelete) {
+    await fetch(`${URL}/posts/${targetDelete}`, {method: 'DELETE'})
+      .then(response => response.json())
+      renderPost()
+  }
+
 })
 
 function loadComments (comments) {
