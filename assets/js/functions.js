6e7b9c0a43150bf6e33be6fdaa362b622e365668
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-08-10 14:53:35 +0200

    Add dynamic content to modal

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -28,11 +28,38 @@ function renderPost () {
 }
 
 document.getElementById('postFeed').addEventListener('click', async (e) => {
-  let target = e.target
   let targetId = target.dataset.id
-  let post = await fetch(`${URL}/posts/${targetId}`)
-  .then(response => response.json())
-  console.log(post);
+  let post = await fetch(`${URL}/posts/${targetId}`).then(response => response.json())
+  let user = await fetch(`${URL}/users/${post.userId}`).then(response => response.json())
+
+  let modalHTML = `
+  <div class="modal-header">
+    <h2 class="modal-title" id="modalTitle">${post.title}</h2>
+    <button
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="modal"
+      aria-label="Close"
+    ></button>
+  </div>
+  <div id="modalBody" class="modal-body">
+    <p>${post.body}</p>
+    <h3>user</h3>
+    <p>${user.name}</p>
+    <a href="mailto:${user.email}">${user.email}</a>
+    <h3>comments</h3>
+    <p>comment 1 (name, body, email)</p>
+    <p>comment 2 (name, body, email)</p>
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-primary">
+      Load comments
+    </button>
+  </div>
+  `
+  
+  document.getElementById('myModal').innerHTML = modalHTML
+
 })
 
 renderPost()
